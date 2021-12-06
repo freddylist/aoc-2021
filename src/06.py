e44191431acc5cef431d7fa11215ccc101ede551
Frederik List <51757856+freddylist@users.noreply.github.com>
2021-12-06 13:12:45 +0100

    Add day 6 solutions

---
+++ src/06.py
@@ -0,0 +1,37 @@
+import inputs
+
+DAYS_1 = 80
+DAYS_2 = 256
+
+GROW_TIME = 2
+REST_TIME = 7
+
+NEW_TIME = GROW_TIME + REST_TIME
+
+input = inputs.get(6)
+initial_fishies = [int(x) for x in input.split(",")]
+#initial_fishies = [3, 4, 3, 1, 2]
+
+offsets = [NEW_TIME - x - 1 for x in initial_fishies]
+
+def make_fishies(days):
+    generations = [1] * NEW_TIME
+
+    for _ in range(days - NEW_TIME):
+        generations.append(generations[-NEW_TIME] + generations[-REST_TIME])
+
+    return generations
+
+fishy_babies = make_fishies(max(DAYS_1, DAYS_2) + max(offsets) + 1)
+
+##########
+# Part 1 #
+##########
+
+print(sum(fishy_babies[DAYS_1 + x] for x in offsets))
+
+##########
+# Part 2 #
+##########
+
+print(sum(fishy_babies[DAYS_2 + x] for x in offsets))
